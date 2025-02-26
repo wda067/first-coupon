7f7a0e39ea4ff80f730a35f34fd06803d157797b
Jo Myeong-geun <mg6359@naver.com>
2025-02-27 02:48:23 +0900

    feat: 쿠폰 발급 메서드에 synchronized 추가

--- src/main/java/com/firstcoupon/service/CouponService.java
+++ src/main/java/com/firstcoupon/service/CouponService.java
@@ -3,6 +3,8 @@ package com.firstcoupon.service;
 import com.firstcoupon.domain.Coupon;
 import com.firstcoupon.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CouponService {
 
     private static final int MAX_COUPON = 100;
+    private static final String COUPON_EXHAUSTED_MESSAGE = "쿠폰이 모두 소진되었습니다.";
 
     private final CouponRepository couponRepository;
 
@@ -18,8 +21,19 @@ public class CouponService {
     public void issueCoupon(Long userId) {
         long count = couponRepository.count();
 
-        if (count > MAX_COUPON) {
-            throw new IllegalStateException("쿠폰이 모두 소진되었습니다.");
+        if (count >= MAX_COUPON) {
+            throw new IllegalStateException(COUPON_EXHAUSTED_MESSAGE);
+        }
+
+        couponRepository.save(new Coupon(userId));
+    }
+
+    @Transactional
+    public synchronized void issueCouponWithSynchronized(Long userId) {
+        long count = couponRepository.count();
+
+        if (count >= MAX_COUPON) {
+            throw new IllegalStateException(COUPON_EXHAUSTED_MESSAGE);
         }
 
         couponRepository.save(new Coupon(userId));
