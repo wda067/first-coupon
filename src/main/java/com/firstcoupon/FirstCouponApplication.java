4668e64053713a18faa6e6315124f7da0d2ae108
Jo Myeong-geun <mg6359@naver.com>
2025-02-27 01:02:01 +0900

    first commit

---
+++ src/main/java/com/firstcoupon/FirstCouponApplication.java
@@ -0,0 +1,13 @@
+package com.firstcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstcoupon/FirstCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
