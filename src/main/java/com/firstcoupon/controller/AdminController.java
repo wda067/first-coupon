14459222951102f66a0ffdf928d55681c25746dd
Jo Myeong-geun <mg6359@naver.com>
2025-03-05 02:04:19 +0900

    feat: 쿠폰 관리자 컨트롤러 추가

--- src/main/java/com/firstcoupon/controller/AdminController.java
+++ src/main/java/com/firstcoupon/controller/AdminController.java
@@ -8,20 +8,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService adminService;
 
-    @PostMapping("/admin/coupon")
+    @PostMapping("/coupon")
     public void createCoupon(@RequestBody CouponCreate request) {
         adminService.createCoupon(request);
     }
 
-    @GetMapping("/admin/coupons")
+    @GetMapping("/coupons")
     public List<CouponResponse> getCoupons() {
         return adminService.getCoupons();
     }
