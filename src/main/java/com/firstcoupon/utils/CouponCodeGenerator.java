1ed7207880bef25157c03f550c29c8463ba1fbb4
Jo Myeong-geun <mg6359@naver.com>
2025-03-04 22:34:36 +0900

    refactor: 쿠폰 코드 생성 로직 수정

--- src/main/java/com/firstcoupon/utils/CouponCodeGenerator.java
+++ src/main/java/com/firstcoupon/utils/CouponCodeGenerator.java
@@ -1,13 +1,23 @@
 package com.firstcoupon.utils;
 
-import java.time.LocalDate;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class CouponCodeGenerator {
 
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
     public static String generate() {
-        String date = LocalDate.now().toString().replace("-", "");
-        int random = new Random().nextInt(9999);
-        return "EVENT" + date + String.format("%04d", random);
+        StringBuilder code = new StringBuilder();
+
+        for (int i = 0; i < 12; i++) {
+            code.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+
+        return formatWithHyphens(code.toString());
+    }
+
+    private static String formatWithHyphens(String code) {
+        return code.replaceAll("(.{4})", "$1-").substring(0, 14);  //4자리마다 '-' 추가
     }
 }
