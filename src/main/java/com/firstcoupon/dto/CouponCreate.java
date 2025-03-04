d30fd84f89cb6a630f550fc0ca3c5a4bbb134bee
Jo Myeong-geun <mg6359@naver.com>
2025-03-04 22:36:12 +0900

    fix: DTO에서 LocalDateTime으로 입력받도록 수정

--- src/main/java/com/firstcoupon/dto/CouponCreate.java
+++ src/main/java/com/firstcoupon/dto/CouponCreate.java
@@ -1,5 +1,11 @@
 package com.firstcoupon.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,9 +16,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CouponCreate {
 
+    @NotBlank(message = "쿠폰 이름을 입력해 주세요.")
     private String couponName;
+
+    @NotNull(message = "쿠폰 총 발급 수량을 입력해 주세요.")
+    @Min(value = 1, message = "1개 이상 입력해 주세요.")
     private int totalQuantity;
-    private String expirationDate;
-    private String issueStartTime;
-    private String issueEndTime;
+
+    @NotNull(message = "쿠폰 만료일을 입력해 주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDate expirationDate;
+
+    @NotNull(message = "쿠폰 발급 시작 시간을 입력해 주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime issueStartTime;
+
+    @NotNull(message = "쿠폰 발급 종료 시간을 입력해 주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime issueEndTime;
 }
--- src/main/java/com/firstcoupon/service/AdminService.java
+++ src/main/java/com/firstcoupon/service/AdminService.java
@@ -4,11 +4,9 @@ import com.firstcoupon.domain.Coupon;
 import com.firstcoupon.dto.CouponCreate;
 import com.firstcoupon.dto.CouponResponse;
 import com.firstcoupon.exception.CouponAlreadyExists;
-import com.firstcoupon.exception.InvalidExpirationDate;
 import com.firstcoupon.repository.CouponRepository;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,9 +22,9 @@ public class AdminService {
 
     @Transactional
     public void createCoupon(CouponCreate request) {
-        LocalDate expirationDate = parseLocalDateTime(request.getExpirationDate()).toLocalDate();
-        LocalDateTime issueStartTime = parseLocalDateTime(request.getIssueStartTime());
-        LocalDateTime issueEndTime = parseLocalDateTime(request.getIssueEndTime());
+        LocalDate expirationDate = request.getExpirationDate();
+        LocalDateTime issueStartTime = request.getIssueStartTime();
+        LocalDateTime issueEndTime = request.getIssueEndTime();
 
         boolean exists = couponRepository.existsByCouponNameAndExpirationDate(request.getCouponName(), expirationDate);
         if (exists) {  //같은 이름, 만료일의 쿠폰이 존재
@@ -36,17 +34,6 @@ public class AdminService {
         Coupon coupon = Coupon.create(request.getCouponName(), request.getTotalQuantity(), expirationDate,
                 issueStartTime, issueEndTime);
         couponRepository.save(coupon);
-    }
-
-    private static LocalDateTime parseLocalDateTime(String raw) {
-        LocalDateTime localDateTime;
-
-        try {  //String -> LocalDateTime 변환
-            localDateTime = LocalDateTime.parse(raw);
-        } catch (DateTimeParseException e) {
-            throw new InvalidExpirationDate();
-        }
-        return localDateTime;
     }
 
     public List<CouponResponse> getCoupons() {
