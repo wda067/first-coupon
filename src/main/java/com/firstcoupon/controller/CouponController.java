8106170478bde5426e54dc6f29c6cc927a8aaa51
Jo Myeong-geun <mg6359@naver.com>
2025-03-05 02:04:43 +0900

    feat: 쿠폰 사용자 컨트롤러 추가

--- src/main/java/com/firstcoupon/controller/CouponController.java
+++ src/main/java/com/firstcoupon/controller/CouponController.java
@@ -1,19 +1,29 @@
 package com.firstcoupon.controller;
 
+import com.firstcoupon.dto.CouponIssue;
 import com.firstcoupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/coupon")
 public class CouponController {
 
     private final CouponService couponService;
 
-    @PostMapping("/coupon/issue/{userId}")
-    public void issueCoupon(@PathVariable Long userId) {
-        couponService.issueCouponWithKafka(userId);
+    @PostMapping("/issue")
+    public void issueCoupon(@RequestBody CouponIssue request) {
+        couponService.issueCouponWithKafka(request);
+    }
+
+    @PostMapping("/use")
+    public void useCoupon(@RequestParam Long userId) {
+        couponService.useCoupon(userId);
     }
 }
