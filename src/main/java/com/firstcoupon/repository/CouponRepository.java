6b1b9631fc2ae873c3bbcfbf9e4a2812d6b2d3cc
Jo Myeong-geun <mg6359@naver.com>
2025-03-04 21:45:20 +0900

    feat: 쿠폰 존재 유무와 코드로 찾는 메서드 추가

--- src/main/java/com/firstcoupon/repository/CouponRepository.java
+++ src/main/java/com/firstcoupon/repository/CouponRepository.java
@@ -1,10 +1,13 @@
 package com.firstcoupon.repository;
 
 import com.firstcoupon.domain.Coupon;
+import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
-    Optional<Coupon> findByUserId(Long userId);
+    boolean existsByCouponNameAndExpirationDate(String couponName, LocalDate expirationDate);
+
+    Optional<Coupon> findByCode(String code);
 }
