fd965f7db893a7f3d03e57d1f831fc9ea43c01fd
Jo Myeong-geun <mg6359@naver.com>
2025-03-12 18:27:04 +0900

    refactor: 이미지 풀 정책 수정

--- src/test/java/com/firstcoupon/config/kafka/KafkaTestContainer.java
+++ src/test/java/com/firstcoupon/config/kafka/KafkaTestContainer.java
@@ -13,8 +13,7 @@ public class KafkaTestContainer {
     private static final KafkaContainer KAFKA_CONTAINER;
 
     static {
-        KAFKA_CONTAINER = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
-                .withImagePullPolicy(PullPolicy.alwaysPull());
+        KAFKA_CONTAINER = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
         KAFKA_CONTAINER.start();
         System.setProperty("spring.kafka.bootstrap-servers", KAFKA_CONTAINER.getBootstrapServers());
     }
