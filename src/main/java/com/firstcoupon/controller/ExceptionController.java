2e87da8019df96ef31a1ba88ef32455bcbfe5875
Jo Myeong-geun <mg6359@naver.com>
2025-03-12 18:26:19 +0900

    feat: 컨트롤러 어드바이스에 Spring Validation 예외 추가

--- src/main/java/com/firstcoupon/controller/ExceptionController.java
+++ src/main/java/com/firstcoupon/controller/ExceptionController.java
@@ -2,11 +2,21 @@ package com.firstcoupon.controller;
 
 import com.firstcoupon.dto.ErrorResponse;
 import com.firstcoupon.exception.CustomException;
+import com.firstcoupon.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse exceptionHandler(MethodArgumentNotValidException exception) {
+        return new ErrorResponse(ErrorCode.BAD_REQUEST, exception.getFieldErrors());
+    }
 
     @ExceptionHandler(CustomException.class)
     public ErrorResponse exceptionHandler(CustomException e) {
